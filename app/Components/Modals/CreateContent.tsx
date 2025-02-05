f013d6ace9e8cfa9bcdcefb53879ec4c8df25c0c
amanxrawat <amanxrawat.93@gmail.com>
2025-02-05 22:31:59 +0530

    - Added placeholder for create content modal

--- app/Components/Modals/CreateContent.tsx
+++ app/Components/Modals/CreateContent.tsx
@@ -78,7 +78,7 @@ function CreateContent() {
           value={title}
           name="title"
           onChange={handleChange("title")}
-          placeholder="e.g, Watch a video from Fireship."
+          placeholder="e.g, creating home page , New project , etc."
         />
       </div>
       <div className="input-control">
@@ -89,7 +89,7 @@ function CreateContent() {
           name="description"
           id="description"
           rows={4}
-          placeholder="e.g, Watch a video about Next.js Auth"
+          placeholder="e.g, adding authentication , adding sidebar , etc."
         ></textarea>
       </div>
       <div className="input-control">
